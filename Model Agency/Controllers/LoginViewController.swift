bdc630c965538efdf0c5822b24e639414b50a631
Yuraist <yura.ist@icloud.com>
2018-08-21 20:25:27 +0300

    Initial Model Agency app commit

---
+++ Model Agency/Controllers/LoginViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Model Agency
+//
+//  Created by Юрий Истомин on 21/08/2018.
+//  Copyright © 2018 Yuri Istomin. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+    navigationItem.title = "Войти"
+  }
+
+
+}
+
