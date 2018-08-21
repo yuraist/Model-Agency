95433f1305ac576b6dabd86ff994f835d9f9c978
Yuraist <yura.ist@icloud.com>
2018-08-21 20:09:50 +0300

    Initial Commit

---
+++ Model Agency/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Model Agency
+//
+//  Created by Юрий Истомин on 21/08/2018.
+//  Copyright © 2018 Yuri Istomin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
